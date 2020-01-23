0ac4b120caee54fd3fae1342a6ba3821f0f3ffb7
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2020-01-23 18:59:05 -0200

    Automatic update of the EuropePMC list of classics

---
+++ twitterbot/api_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+oauth = "eJuOWjXnt6VJDo3Xq9XkfnLlw"
+oauth_secret = "3pqIZ0A7SV6a3kZ2VIvDQSzOARAWI9xal1n7Kxm0pRYm0AXxbu"
+
+token = "794138010879148032-UPmqcpgwiX7U8DcaoVSbXW5r6X16C64"
+token_secret = "Zx0UeAbcmbJuJUWcOtrjNco7LwJH39RzvWKKBdjYF4TtM"
